94901bfc605aa141915beb3de8b0234bfaacb337
usmonjon <umediagram@gmail.com>
2024-06-07 07:49:59 +0500

    Homework #3

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// masala 9
+// {
+//     let kirtiishA = +prompt("A sonini kiriting");
+//     let kirtiishB = +prompt("B sonini kiriting");
+
+//     if (kirtiishA > kirtiishB) {
+//         const temp = kirtiishA;
+//         kirtiishA = kirtiishB;
+//         kirtiishB = temp;
+//     }
+
+//     alert(`A: ${kirtiishA}, B: ${kirtiishB}`);
+// }
+
+// masala 10
+// {
+//     let kiritishA = +prompt("A sonin kiriting");
+//     let kiritishB = +prompt("B sonin kiriting");
+
+//     if (kiritishA === kiritishB) {
+//         kiritishA += kiritishB;
+//         kiritishB = kiritishA;
+//     } else {
+//         kiritishA = 0;
+//         kiritishB = 0;
+//     }
+
+//     alert(`A: ${kirtiishA}, B: ${kirtiishB}`);
+// }
